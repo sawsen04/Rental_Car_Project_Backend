b176688d670eead02f00ff2415a27c7a4e4087c4
sawsen04 <sawsenayari87@gmail.com>
2024-02-22 03:22:20 +0100

    order

--- routes/admin/getOrders.js
+++ routes/admin/getOrders.js
@@ -2,7 +2,7 @@ const Order = require("../../models/Order");
 module.exports = async (req, res) => {
   try {
     let orders = await Order.find();
-    res.status(200).json({ status: true, data: orders });
+    res.status(200).json({ status: true, data: orders }).populate("userId");
   } catch (error) {
     if (error) {
       console.log(error);
