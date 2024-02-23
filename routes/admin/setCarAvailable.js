56e238d8efeded61e3a7ad1c0624fecc89f3d021
sawsen04 <sawsenayari87@gmail.com>
2024-02-23 22:55:30 +0100

    finish order

--- routes/admin/setCarAvailable.js
+++ routes/admin/setCarAvailable.js
@@ -2,15 +2,19 @@ const Order = require("../../models/Order");
 const Car = require("../../models/Car");
 module.exports = async (req, res) => {
   try {
-    let { carId } = req.params;
+    let { orderId, carId } = req.params;
     // await Order.findByIdAndDelete(orderId);
-    let update = await Car.findByIdAndUpdate(carId, {
+    await Car.findByIdAndUpdate(carId, {
       $set: {
         isAvailable: true,
+      },
+    });
+    await Order.findByIdAndUpdate(orderId, {
+      $set: {
         isFinished: true,
       },
     });
-    res.status(200).json({ status: true, message: update });
+    res.status(200).json({ status: true, message: "ok" });
   } catch (error) {
     if (error) {
       console.log(error);
